c83b5b8403fb198461a60bebc503c9e08accce42
HyehyeonLee <july4th_@naver.com>
2022-05-25 19:16:22 +0900

    2022-05-25 p161

--- src/main/java/com/hh/awsproject/springboot/web/PostsApiController.java
+++ src/main/java/com/hh/awsproject/springboot/web/PostsApiController.java
@@ -1,29 +1,44 @@
 package com.hh.awsproject.springboot.web;
 
 import com.hh.awsproject.springboot.service.posts.PostsService;
+import com.hh.awsproject.springboot.web.dto.PostsListResponseDto;
 import com.hh.awsproject.springboot.web.dto.PostsResponseDto;
 import com.hh.awsproject.springboot.web.dto.PostsSaveRequestDto;
 import com.hh.awsproject.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @RestController
 public class PostsApiController {
+
     private final PostsService postsService;
 
     @PostMapping("/api/v1/posts")
-    public Long save(@RequestBody PostsSaveRequestDto requestDto){ //글 등록
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
     }
 
     @PutMapping("/api/v1/posts/{id}")
-    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){ //수정
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
         return postsService.update(id, requestDto);
     }
 
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
+    }
+
     @GetMapping("/api/v1/posts/{id}")
-    public PostsResponseDto findById(@PathVariable Long id){
+    public PostsResponseDto findById(@PathVariable Long id) {
         return postsService.findById(id);
+    }
+
+    @GetMapping("/api/v1/posts/list")
+    public List<PostsListResponseDto> findAll() {
+        return postsService.findAllDesc();
     }
 }
